839f0aad4cd9ca5e08dd29755228753e45edd1d8
rah1236 <39041279+rah1236@users.noreply.github.com>
2025-03-06 14:53:24 -0800

    please god in heaven work

--- IV_Tracer.py
+++ IV_Tracer.py
@@ -8,7 +8,7 @@ psu_address = 'USB0::0x2A8D::0x1102::MY12345678::INSTR'  # EDU36311A
 dmm_address = 'USB0::0x2A8D::0x1401::MY12345678::INSTR'  # EDU34450A
 
 # Create resource manager and connect to instruments
-rm = pyvisa.ResourceManager('@py')
+rm = pyvisa.ResourceManager()
 psu = rm.open_resource(psu_address)
 dmm = rm.open_resource(dmm_address)
 
@@ -19,8 +19,6 @@ dmm.write('*RST')
 # Data storage
 currents_dmm = []
 voltages_dmm = []
-currents_src = []
-voltages_src = []
 
 # Test parameters
 current_limit = 0.5  # Amps
@@ -28,32 +26,28 @@ final_voltage = 5.0  # Volts
 voltage_step = 0.2   # Volts
 
 # Configure PSU
-psu.write(f'SOUR1:CURR {current_limit}')
-psu.write('SOUR1:VOLT 0')
+psu.write(f'CURRent {current_limit}, (@1)')
+psu.write('VOLTage 0, (@1)')
 
 # Voltage sweep
 for voltage in np.arange(0, final_voltage + voltage_step, voltage_step):
     voltage = round(voltage, 2)
     
     # Set voltage and enable output
-    psu.write(f"SOUR1:VOLT {voltage}")
-    psu.write('OUTP1 ON')
+    psu.write(f"VOLTage {voltage}, (@1)")
+    psu.write('OUTPut 1, (@1)')
     
     # Wait for measurement to settle
-    time.sleep(0.2)
+    time.sleep(0.75)
     
     # Measure with DMM
     currents_dmm.append(float(dmm.query("MEAS:CURR:DC?")))
     voltages_dmm.append(float(dmm.query("MEAS:VOLT:DC?")))
     
-    # Measure with PSU
-    currents_src.append(float(psu.query("MEAS:CURR? CH1")))
-    voltages_src.append(float(psu.query("MEAS:VOLT? CH1")))
-    
     print(f"V={voltage}V, I={currents_dmm[-1]:.4f}A")
 
 # Turn off output
-psu.write('OUTP1 OFF')
+psu.write('OUTPut 0, (@1)')
 
 # Plot results
 plt.figure(figsize=(12, 5))
